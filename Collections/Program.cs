ed7a3b14ac977c6d6e9b0d43db1e2f1d1713dfbf
BERNA NUR TAŞDEMİR <17050211051@ybu.edu.tr>
2021-03-09 12:54:01 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* string[] isimler = new string[] {"Berna","Büşra","Emine","Sueda" };
+             Console.WriteLine(isimler[0]);
+            */
+            //ctrl+k ctrl+c //ctrl+k ctrl u
+
+            List<string> İsimler2 = new List<string> { "Berna", "Büşra", "Emine", "Sueda" };
+            Console.WriteLine(İsimler2[0]);
+            İsimler2.Add("Fethiye");
+            Console.WriteLine(İsimler2[4]);
+
+            
+
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //CRUD create read update delete
+        //encapsulation
+        public void Add(Product product)//(string ad)
+        {
+
+            Console.WriteLine();
+        }
+    }
+}
