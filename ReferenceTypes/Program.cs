b3075665b2e0ca5d6b818c7a62d357f2d6a231d5
BERNA NUR TAŞDEMİR <17050211051@ybu.edu.tr>
2021-03-10 10:23:53 +0300

    09.03.2021

---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+            Customer customer = new Customer();
+            customer.FirstName = "Berna";
+            customer.CreditCardNumber = 456987;
+
+            Employee employee = new Employee();
+            Person person3 = customer;
+            customer.FirstName = "Beyza";
+            PersonManager personManager = new PersonManager();
+            personManager.Add(person3);
+
+            Console.WriteLine(((Customer)person3).CreditCardNumber);
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+
+    //base class
+    class Customer:Person
+    {
+        public int CreditCardNumber { get; set; }
+
+    }
+    class Employee:Person
+    {
+        public int EmployeeNumber { get; set; }
+
+    }
+
+    class PersonManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+
+    }
+}
